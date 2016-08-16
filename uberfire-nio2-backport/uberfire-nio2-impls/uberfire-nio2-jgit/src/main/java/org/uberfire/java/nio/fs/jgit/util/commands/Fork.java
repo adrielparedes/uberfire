c112c9fbf94130b0c8374479be313af052c0fcea
Adriel Paredes <adriel.paredes@gmail.com>
2016-08-16 13:53:41 -0300

    [fork]: Fork command added, is an abstraction of clone command but it stores some information about the fork

---
+++ uberfire-nio2-backport/uberfire-nio2-impls/uberfire-nio2-jgit/src/main/java/org/uberfire/java/nio/fs/jgit/util/commands/Fork.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2016 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.uberfire.java.nio.fs.jgit.util.commands;
+
+import java.io.File;
+import java.util.Optional;
+
+import org.eclipse.jgit.lib.RepositoryCache;
+import org.eclipse.jgit.transport.CredentialsProvider;
+import org.uberfire.java.nio.fs.jgit.util.JGitUtil;
+
+import static org.eclipse.jgit.util.FS.*;
+
+public class Fork extends GitCommand {
+
+    private File parentFolder;
+    private final String source;
+    private final String target;
+    private CredentialsProvider credentialsProvider;
+
+    public Fork( File parentFolder,
+                 String source,
+                 String target,
+                 CredentialsProvider credentialsProvider ) {
+        this.parentFolder = parentFolder;
+        this.source = source;
+        this.target = target;
+        this.credentialsProvider = credentialsProvider;
+    }
+
+    @Override
+    public Optional<Void> execute() {
+
+        final File gitSource = this.getGitRepository( parentFolder, source );
+        final File gitClonedFolder = new File( parentFolder, target );
+        JGitUtil.cloneRepository( gitClonedFolder, gitSource.getPath(), true, credentialsProvider );
+        return Optional.empty();
+    }
+
+    private File getGitRepository( File parentFolder,
+                                   String repo ) {
+        final File repoFolder = new File( parentFolder, repo );
+        return RepositoryCache.FileKey.resolve( repoFolder, DETECTED );
+    }
+}
---
+++ uberfire-nio2-backport/uberfire-nio2-impls/uberfire-nio2-jgit/src/test/java/org/uberfire/java/nio/fs/jgit/JGitForkTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2016 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.uberfire.java.nio.fs.jgit;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.transport.CredentialsProvider;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.uberfire.java.nio.fs.jgit.util.JGitUtil;
+import org.uberfire.java.nio.fs.jgit.util.commands.Fork;
+
+import static org.eclipse.jgit.api.ListBranchCommand.ListMode.ALL;
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.uberfire.java.nio.fs.jgit.util.JGitUtil.*;
+
+public class JGitForkTest extends AbstractTestInfra {
+
+    private Logger logger = LoggerFactory.getLogger( JGitForkTest.class );
+
+    @Test
+    public void testToForkSuccess() throws IOException {
+        final File parentFolder = createTempDirectory();
+
+        final File gitSource = new File( parentFolder, "source.git" );
+        final Git origin = JGitUtil.newRepository( gitSource, true );
+
+        commit( origin, "user_branch", "name", "name@example.com", "commit!", null, null, false, new HashMap<String, File>() {{
+            put( "file2.txt", tempFile( "temp2222" ) );
+        }} );
+        commit( origin, "master", "name", "name@example.com", "commit", null, null, false, new HashMap<String, File>() {{
+            put( "file.txt", tempFile( "temp" ) );
+        }} );
+        commit( origin, "master", "name", "name@example.com", "commit", null, null, false, new HashMap<String, File>() {{
+            put( "file3.txt", tempFile( "temp3" ) );
+        }} );
+
+        new Fork( parentFolder, "source.git", "target.git", CredentialsProvider.getDefault() ).execute();
+
+        final File gitCloned = new File( parentFolder, "target.git" );
+        final Git cloned = Git.open( gitCloned );
+
+        assertThat( cloned ).isNotNull();
+
+        assertThat( branchList( cloned, ALL ) ).hasSize( 4 );
+
+        assertThat( branchList( cloned, ALL ).get( 0 ).getName() ).isEqualTo( "refs/heads/master" );
+        assertThat( branchList( cloned, ALL ).get( 1 ).getName() ).isEqualTo( "refs/heads/user_branch" );
+        assertThat( branchList( cloned, ALL ).get( 2 ).getName() ).isEqualTo( "refs/remotes/origin/master" );
+        assertThat( branchList( cloned, ALL ).get( 3 ).getName() ).isEqualTo( "refs/remotes/origin/user_branch" );
+
+    }
+
+}
